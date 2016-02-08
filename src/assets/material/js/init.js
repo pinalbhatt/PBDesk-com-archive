e56da36c458a57fd3e95ec67858a558ea504c834
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2016-02-08 15:11:36 -0500

    Material design

--- src/assets/material/js/init.js
+++ src/assets/material/js/init.js
@@ -11,18 +11,19 @@ $(document).ready(function() {
                  SKILL BAR 
     **************************************************************************/
 
-      $(".determinate").each(function(){
-          var width = $(this).text();
-          $(this).css("width", width)
-            .empty()
-            .append('<i class="fa fa-circle"></i>');                
-      });
-  
+    $(".determinate").each(function(){
+      var width = $(this).text();
+      $(this).css("width", width)
+        .empty()
+        .append('<i class="fa fa-circle"></i>');                
+    });
+
+
     /**************************************************************************
              BLOG POST 
     **************************************************************************/
   
-      jQuery(window).on('load', function(){ var $ = jQuery;
+    jQuery(window).on('load', function(){ var $ = jQuery;
         $('.blog').masonry({
           itemSelector: '.blog-post',
           columnWidth: '.blog-post',
@@ -50,74 +51,123 @@ $(document).ready(function() {
     var wow = new WOW({ mobile: false });
     wow.init();
 
-
     /***************************************************************************
           CONTACT FORM
     ***************************************************************************/
 
-      $("#contactForm").validator().on("submit", function (event) {
-      if (event.isDefaultPrevented()) {
-          // handle the invalid form...
-          formError();
-          submitMSG(false, "Did you fill in the form properly?");
-      } else {
-          // everything looks good!
-          event.preventDefault();
-          submitForm();
-      }
-  });
-
-
-  function submitForm(){
-      // Initiate Variables With Form Content
-      var name = $("#name").val();
-      var email = $("#email").val();
-      var message = $("#message").val();
-
-      $.ajax({
-          type: "POST",
-          url: "process.php",
-          data: "name=" + name + "&email=" + email + "&message=" + message,
-          success : function(text){
-              if (text == "success"){
-                  formSuccess();
-              } else {
-                  formError();
-                  submitMSG(false,text);
-              }
-          }
-      });
-  }
-
-  function formSuccess(){
-      $("#contactForm")[0].reset();
-      submitMSG(true, "Message Sent!")
-  }
-
-  function formError(){
-      $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+    $("#contactForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            formError();
+            submitMSG(false, "Did you fill in the form properly?");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            submitForm();
+        }
+    });
+    function submitForm(){
+    // Initiate Variables With Form Content
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
+        
+        $.ajax({
+            type: "POST",
+            url: "process.php",
+            data: "name=" + name + "&email=" + email + "&message=" + message,
+            success : function(text){
+                if (text == "success"){
+                    formSuccess();
+                } else {
+                    formError();
+                    submitMSG(false,text);
+                }
+            }
+        });
+    }
+    function formSuccess(){
+        $("#contactForm")[0].reset();
+        submitMSG(true, "Message Sent!")
+    }
+    function formError(){
+        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
         function(){
-          $(this).removeClass();
-      });
-  }
+            $(this).removeClass();
+        });
+    }
+    function submitMSG(valid, msg){
+        if(valid){
+            var msgClasses = "h3 text-center fadeInUp animated text-success";
+        } else {
+            var msgClasses = "h3 text-center text-danger";
+        }
+        $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+    
+    
+    /**************************************************************************
+       Projects
+    **************************************************************************/
+    $('#portfolio-item').mixItUp();
+    
+    $('.sa-view-project-detail').on('click', function(event) {
+        event.preventDefault();
+        var href          = $(this).attr('href') + ' ' + $(this).attr('data-action'),
+            dataShow      = $('#project-gallery-view'),
+            dataShowMeta  = $('#project-gallery-view meta'),
+            dataHide      = $('#portfolio-item'),
+            preLoader     = $('#loader'),
+            backBtn       = $('#back-button'),
+            filterBtn     = $('#filter-button');
 
-  function submitMSG(valid, msg){
-      if(valid){
-          var msgClasses = "h3 text-center fadeInUp animated text-success";
-      } else {
-          var msgClasses = "h3 text-center text-danger";
-      }
-      $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-  }
+        dataHide.animate( { 'marginLeft':'-120%' }, { duration: 400, queue: false } );
+        filterBtn.animate( { 'marginLeft':'-120%' }, { duration: 400, queue: false } );
+        dataHide.fadeOut(400);
+        filterBtn.fadeOut(400);
+        setTimeout( function() { preLoader.show(); }, 400);
+        setTimeout( function() {
+            dataShow.load( href, function() {
+                dataShowMeta.remove();
+                preLoader.hide();
+                dataShow.fadeIn(600);
+                backBtn.fadeIn(600);
+            });
+        },800);
+    });
+
+    $('#back-button').on('click', function(event) {
+        event.preventDefault();
+        var dataShow    = $('#portfolio-item'),
+            dataHide    = $('#project-gallery-view'),
+            filterBtn   = $('#filter-button');
+
+        $("[data-animate]").each( function() {
+            $(this).addClass($(this).attr('data-animate'));
+        });
+
+        dataHide.fadeOut(400);
+        $(this).fadeOut(400);
+        setTimeout(function(){
+            dataShow.animate( { 'marginLeft': '0' }, { duration: 400, queue: false } );
+            filterBtn.animate( { 'marginLeft': '0' }, { duration: 400, queue: false } );
+            dataShow.fadeIn(400);
+            filterBtn.fadeIn(400);
+        },400);
+        setTimeout(function(){
+            dataShow.find('.fadeInRight, .fadeInLeft, .fadeInUp, .fadeInDown').removeClass('fadeInRight').removeClass('fadeInLeft').removeClass('fadeInUp').removeClass('fadeInDown');
+        },1500);
+    });
+
 });
 
-/***************************************************************************
-              MAP
-***************************************************************************/
-
-   /* google.maps.event.addDomListener(window, 'load', init);
+    /***************************************************************************
+                MAP
+    ***************************************************************************/
+    
+    google.maps.event.addDomListener(window, 'load', init);
     function init() {
-      var mapOptions = {
+        var mapOptions = {
           zoom: 17,
           scrollwheel: false, 
           navigationControl: false,
@@ -142,8 +192,6 @@ $(document).ready(function() {
           position: new google.maps.LatLng(24.906308,91.870413),
           map: map,
           title: '24 Golden Tower (2nd floor), Amborkhana, Sylhet.!'
-          
       });
-    }*/
+    }
 
-  
